600cd8b6441f83d66a63ddf3834737171476a953
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-08-05 12:58:16 +0530

    Navigate Component
    Added demos for using <Navigate /> component to setup re-directs and using a useNavigate hook wrapper to navigate to custom URL(s) or navigate back & forth the user's browser history

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 function App() {
     
+  // The <Navigate /> component is a wrapper around the useNavigate hook 
+  // that lets us navigate programmatically, to URLs, or even go back down the user’s history
+
+  const navigate = useNavigate();
+
   return(
 
     <div>
@@ -23,6 +28,21 @@ function App() {
         </ul>
 
       </nav>
+
+      {/* Re-Directing Logic using <Navigate /> Component */}
+      {/* The user will always be re-directed to "/profile/default" when they try to navigate to the Main Page "/" */}
+
+      {/* <Navigate to="/profile/default" /> */}
+
+      {/* Using useNavigate hook to programmatically navigate to a custom URL or go back or forth the user's browser history */}
+
+      <button onClick={ () => navigate("/profile/default") }>Navigate Test - Custom - "/profile/default"</button>
+
+      <br></br>
+      <br></br>
+
+      <button onClick={ () => navigate(-1) }>Navigate Test - Back - (-1)</button><br></br>
+      <button onClick={ () => navigate(+1) }>Navigate Test - Forward - (+1)</button>
 
     </div>
   );
